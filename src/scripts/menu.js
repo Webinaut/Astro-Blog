f2240213538b9f74fdb183c7e15840fff5d28e97
Martin <slackgt@bigpond.com>
2024-07-07 20:25:23 +1000

    3rd Commit

---
+++ src/scripts/menu.js
@@ -0,0 +1,13 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+  document.querySelector(".nav-links").classList.toggle("expanded");
+});
+
+// Set active class on nav when the page is active
+const nav = document.querySelector(".nav-links");
+const navLinks = nav.querySelectorAll("a");
+const currentURL = window.location.href;
+navLinks.forEach((link) => {
+  if (link.href === currentURL) {
+    link.classList.add("active");
+  }
+});
