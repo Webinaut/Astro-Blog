6d9cd25f14808a4f10d09050f856a63099ea1139
Martin <slackgt@bigpond.com>
2024-07-09 14:12:12 +1000

    5th

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,9 +1,9 @@
-console.log("The menu script has loaded");
 document.querySelector(".hamburger").addEventListener("click", () => {
   document.querySelector(".nav-links").classList.toggle("expanded");
 });
 
 // Set active class on nav when the page is active
+
 const nav = document.querySelector(".nav-links");
 const navLinks = nav.querySelectorAll("a");
 const currentURL = window.location.href;
